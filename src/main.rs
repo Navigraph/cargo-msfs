d20f7030702cd5ebd7d04f1347c229e5a411a58c
Jack Lavigne <jacklavigne00@gmail.com>
2024-10-29 23:20:43 +0100

    feat: initial commit

---
+++ src/main.rs
@@ -0,0 +1,261 @@
+use std::{
+    io::BufReader,
+    path::PathBuf,
+    process::{Command, Stdio},
+    time::Duration,
+};
+
+use anyhow::{Context, Result};
+use cargo_metadata::Message;
+use clap::{builder::ArgPredicate, Parser, ValueEnum};
+use console::style;
+use directories::ProjectDirs;
+use indicatif::{ProgressBar, ProgressStyle};
+use sdk::{
+    get_installed_sdk_version, get_latest_sdk_release, get_latest_sdk_version, get_sdk_path,
+    get_wasi_sysroot_path, install_latest_sdk, remove_sdk_version,
+};
+
+/// SDK info and download utility
+mod sdk;
+
+/// A specific version of MSFS
+#[derive(Debug, PartialEq, Eq, Clone, Copy, ValueEnum)]
+enum SimulatorVersion {
+    Msfs2020,
+    Msfs2024,
+}
+
+#[derive(Debug, Clone, Copy, ValueEnum)]
+enum CommandType {
+    /// Installs the SDK for a specified MSFS version
+    Install,
+    /// Removes the SDK for a specified MSFS version
+    Remove,
+    /// Updates the SDK for a specified MSFS version
+    Update,
+    /// Builds a crate for a specified MSFS version
+    Build,
+    /// Gets info on installed SDKs
+    Info,
+}
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// The command to run
+    #[arg(value_enum)]
+    command: CommandType,
+    /// The version of MSFS to run for. This is optional if the command type is info
+    #[arg(value_enum, required_if_eq_any([
+        ("command", "install"),
+        ("command", "remove"),
+        ("command", "update"),
+        ("command", "build"),
+    ]))]
+    msfs_version: Option<SimulatorVersion>,
+}
+
+/// Formats a string containing the installed SDK version of a given sim
+///
+/// Example: `MSFS2024 SDK version X.X.X is installed` or `MSFS 2024 SDK is not installed`
+///
+/// * `simulator_version` - The simulator version to format for
+fn format_version_string(simulator_version: SimulatorVersion) -> Result<String> {
+    let root_string = format!(
+        "MSFS {} SDK",
+        if simulator_version == SimulatorVersion::Msfs2020 {
+            "2020"
+        } else {
+            "2024"
+        }
+    );
+
+    if let Some(installed_version) = get_installed_sdk_version(simulator_version)? {
+        Ok(format!(
+            "{} version {} is installed, latest available version is {}",
+            root_string,
+            style(installed_version).bold(),
+            style(get_latest_sdk_version(simulator_version)?).bold()
+        ))
+    } else {
+        Ok(format!("{} is not installed", root_string))
+    }
+}
+
+/// Gets the directory that can be used for data
+fn get_data_dir() -> Result<PathBuf> {
+    Ok(ProjectDirs::from("", "", "cargo-msfs")
+        .context("could not get project dir")?
+        .data_dir()
+        .to_path_buf())
+}
+
+/// Logs info
+fn print_info(message: &str) {
+    println!("{} {}", style("[INFO]").cyan(), message);
+}
+
+/// Logs success
+fn print_success(message: &str) {
+    println!("{} {}", style("[SUCCESS]").green(), message);
+}
+
+/// Logs a step
+fn print_step(step_number: u8, num_steps: u8, message: &str) {
+    if step_number == num_steps {
+        println!("{} {}", style("{step_number}/{num_steps}").green(), message);
+    } else {
+        println!(
+            "{} {}",
+            style("{step_number}/{num_steps}").yellow(),
+            message
+        );
+    }
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    match args.command {
+        CommandType::Install => {
+            let sim_version = args.msfs_version.context("msfs version is not present")?;
+            let installed_version = get_installed_sdk_version(sim_version)?;
+            if installed_version.is_some() {
+                print_info("SDK for simulator version is already installed. To update it, run with the update command");
+                return Ok(());
+            }
+
+            print_info("Downloading and installing SDK...");
+            // Create the progress bar. Since we won't know the full length until the callback, initialize with 0
+            let progress_bar = ProgressBar::new(0);
+            progress_bar.set_style(
+                ProgressStyle::with_template(
+                    "{spinner:.green} [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} (ETA {eta})",
+                )
+                .unwrap()
+                .progress_chars("#>-"),
+            );
+            progress_bar.enable_steady_tick(Duration::from_millis(100));
+            install_latest_sdk(
+                sim_version,
+                Some(|downloaded, total| {
+                    if progress_bar.length() != Some(total) {
+                        progress_bar.set_length(total);
+                    }
+
+                    progress_bar.set_position(downloaded);
+                }),
+            )?;
+            print_success("SDK installed");
+        }
+        CommandType::Remove => {
+            let sim_version = args.msfs_version.context("msfs version is not present")?;
+            if get_sdk_path(sim_version)?.exists() {
+                remove_sdk_version(sim_version)?;
+                print_success("SDK deleted");
+            } else {
+                print_info("SDK is not installed, nothing to remove");
+            }
+        }
+        CommandType::Update => {
+            let sim_version = args.msfs_version.context("msfs version is not present")?;
+            let latest_release = get_latest_sdk_version(sim_version)?;
+            let installed_version = get_installed_sdk_version(sim_version)?;
+            if installed_version == Some(latest_release) {
+                print_info("Latest SDK is already installed");
+                return Ok(());
+            } else if installed_version.is_none() {
+                print_info("SDK is not installed. To install it, run with the install command");
+                return Ok(());
+            }
+
+            print_info("Downloading and installing SDK...");
+            // Create the progress bar. Since we won't know the full length until the callback, initialize with 0
+            let progress_bar = ProgressBar::new(0);
+            progress_bar.set_style(
+                ProgressStyle::with_template(
+                    "{spinner:.green} [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} (ETA {eta})",
+                )
+                .unwrap()
+                .progress_chars("#>-"),
+            );
+            progress_bar.enable_steady_tick(Duration::from_millis(100));
+            install_latest_sdk(
+                sim_version,
+                Some(|downloaded, total| {
+                    if progress_bar.length() != Some(total) {
+                        progress_bar.set_length(total);
+                    }
+
+                    progress_bar.set_position(downloaded);
+                }),
+            )?;
+            print_success("SDK updated");
+        }
+        CommandType::Build => {
+            let sim_version = args.msfs_version.context("msfs version is not present")?;
+            let sdk_path = get_sdk_path(sim_version)?;
+            let wasi_sysroot_path = get_wasi_sysroot_path(sim_version)?
+                .as_os_str()
+                .to_str()
+                .context("couldn't convert osstr to str")?
+                .to_string();
+            let flags = [
+                "-Cstrip=symbols",
+                "-Clto",
+                "-Ctarget-feature=-crt-static,+bulk-memory",
+                "-Clink-self-contained=no",
+                "-Clink-arg=-l",
+                "-Clink-arg=c",
+                &format!(
+                    "-Clink-arg={}\\lib\\wasm32-wasi\\libclang_rt.builtins-wasm32.a",
+                    wasi_sysroot_path
+                ),
+                "-Clink-arg=-L",
+                &format!(
+                    "-Clink-arg={}\\wasi-sysroot\\lib\\wasm32-wasi",
+                    wasi_sysroot_path
+                ),
+                "-Clink-arg=--export-table",
+                "-Clink-arg=--allow-undefined",
+                "-Clink-arg=--export-dynamic",
+                "-Clink-arg=--export=__wasm_call_ctors",
+                "-Clink-arg=--export=malloc",
+                "-Clink-arg=--export=free",
+                "-Clink-arg=--export=mark_decommit_pages",
+                "-Clink-arg=--export=mallinfo",
+                "-Clink-arg=--export=mchunkit_begin",
+                "-Clink-arg=--export=mchunkit_next",
+                "-Clink-arg=--export=get_pages_state",
+            ];
+            let mut command = Command::new("cargo")
+                .args(&[
+                    "build",
+                    "--release",
+                    "--target",
+                    "wasm32-wasip1",
+                    // "--message-format=json",
+                ])
+                .env("MSFS_SDK", sdk_path)
+                .env("RUSTFLAGS", flags.join(" "))
+                .env("CFLAGS", format!("--sysroot={}", wasi_sysroot_path))
+                // .stdout(Stdio::piped())
+                .spawn()?;
+            // let reader = BufReader::new(command.stdout.take().context("couldn't take stdout")?);
+            // for message in Message::parse_stream(reader) {
+            //     dbg!(message?);
+            // }
+        }
+
+        CommandType::Info => {
+            if args.msfs_version == None || args.msfs_version == Some(SimulatorVersion::Msfs2020) {
+                print_info(&format_version_string(SimulatorVersion::Msfs2020)?);
+            }
+            if args.msfs_version == None || args.msfs_version == Some(SimulatorVersion::Msfs2024) {
+                print_info(&format_version_string(SimulatorVersion::Msfs2024)?);
+            }
+        }
+    }
+
+    Ok(())
+}
---
+++ src/sdk.rs
@@ -0,0 +1,391 @@
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io::{self, Cursor, Read, Write},
+    path::PathBuf,
+};
+
+use anyhow::{anyhow, Context, Result};
+use cab::Cabinet;
+use msi::{Expr, Package, Row, Select};
+use serde::Deserialize;
+use zip::ZipArchive;
+
+use crate::{get_data_dir, SimulatorVersion};
+
+// Root URLs for SDK files for each version of MSFS
+const MSFS2020_SDK_URL: &str = "https://sdk.flightsimulator.com/files/";
+const MSFS2024_SDK_URL: &str = "https://sdk.flightsimulator.com/msfs2024/files/";
+
+// File name of the manifest file located under the root URL
+const MANIFEST_FILE: &str = "sdk.json";
+
+// The download option key for the core SDK installer
+const CORE_INSTALLER_KEY: &str = "SDK Installer (Core)";
+
+// The folder in the SDK MSI to extract from
+const MSFS2020_SDK_EXTRACT_FROM: &str = ".\\MSFS SDK\\WASM\\";
+const MSFS2024_SDK_EXTRACT_FROM: &str = ".\\MSFS 2024 SDK\\WASM\\";
+
+// Local destination folder names for the downloaded SDK
+const MSFS2020_FOLDER_NAME: &str = "msfs2020";
+const MSFS2024_FOLDER_NAME: &str = "msfs2024";
+
+// File name within the local destination folder where the SDK version is stored
+const VERSION_FILE_NAME: &str = "version.txt";
+
+// WASI sysroot location, relative to the SDK installation. Valid for both SDK editions
+const WASI_SYSROOT_PATH: &str = "WASM\\wasi-sysroot";
+
+// Configuration
+const CHUNK_SIZE: u64 = 1024;
+
+/// A downloads "menu option" containing an optional value
+///
+/// For our case, the downloads menu key we are using will always have a Some() value
+#[derive(Debug, Deserialize, Clone)]
+pub struct DownloadsMenuOption {
+    pub value: Option<String>,
+}
+// A specific SDK (game) version
+#[derive(Debug, Deserialize, Clone)]
+pub struct GameVersion {
+    /// A hashmap of menu titles to URLs (among other things, but we only care about URLs here)
+    pub downloads_menu: HashMap<String, DownloadsMenuOption>,
+    /// Vec of SDK versions. Latest is always the last entry
+    pub release_notes: Vec<String>,
+}
+/// The manifest of available SDK versions
+#[derive(Debug, Deserialize, Clone)]
+pub struct SdkManifest {
+    pub game_versions: Vec<GameVersion>,
+}
+
+/// Extracts the long file name from a string containing both short and long. This works for strings that are only the long file name as well
+///
+/// See https://learn.microsoft.com/en-us/windows/win32/msi/filename
+///
+/// * `string` - The string to parse from. Example value: `vacirzcc.h|WASM_Static_Library.h`
+fn get_long_file_name(string: &str) -> Result<&str> {
+    string
+        .split("|")
+        .last()
+        .context("couldn't get long file name")
+}
+
+/// Recursively traverses the directories and gets the full path of a directory entry
+///
+/// Note: There's not really a type safe way to guarantee in the function signature the row is actually a full Directory row with proper values
+///
+/// See https://learn.microsoft.com/en-us/windows/win32/msi/directory-table
+///
+/// * `directory` - The directory key (DirectoryDir)
+/// * `directories` - A vec of rows from the directory table
+fn get_directory_parent<'a>(directory: &'a str, directories: &'a Vec<Row>) -> Result<PathBuf> {
+    let row = directories
+        .iter()
+        .find(|d| d["Directory"].as_str() == Some(directory))
+        .context("couldn't find source row")?;
+
+    if let Some(parent) = row["Directory_Parent"].as_str() {
+        let parent_path_buf = get_directory_parent(parent, directories)?;
+        Ok(parent_path_buf.join(get_long_file_name(
+            row["DefaultDir"]
+                .as_str()
+                .context("couldn't get directory name")?,
+        )?))
+    } else {
+        // root!
+        Ok(PathBuf::from(""))
+    }
+}
+
+/// Gets the latest SDK version information for the given simulator
+///
+/// * `version` - The simulator version to get for
+pub fn get_latest_sdk_release(version: SimulatorVersion) -> Result<GameVersion> {
+    let response = reqwest::blocking::get(format!(
+        "{}{}",
+        if version == SimulatorVersion::Msfs2020 {
+            MSFS2020_SDK_URL
+        } else {
+            MSFS2024_SDK_URL
+        },
+        MANIFEST_FILE
+    ))?
+    .text()?;
+
+    let manifest = serde_json::from_str::<SdkManifest>(&response)?;
+
+    let latest_sdk = manifest
+        .game_versions
+        .first()
+        .context("can't find game version for SDK")?;
+
+    Ok(latest_sdk.clone())
+}
+
+/// Gets the latest SDK version string for the given simulator
+///
+/// Note: This difers from get_latest_sdk_release, as that returns a struct with extra data
+///
+/// * `version` - The simulator version to get for
+pub fn get_latest_sdk_version(version: SimulatorVersion) -> Result<String> {
+    Ok(get_latest_sdk_release(version)?
+        .release_notes
+        .last()
+        .context("no available sdk version")?
+        .to_string())
+}
+
+/// Gets the desired path for the given simulator
+///
+/// * `version` The simulator version to get the path for
+pub fn get_sdk_path(version: SimulatorVersion) -> Result<PathBuf> {
+    Ok(
+        get_data_dir()?.join(if version == SimulatorVersion::Msfs2020 {
+            MSFS2020_FOLDER_NAME
+        } else {
+            MSFS2024_FOLDER_NAME
+        }),
+    )
+}
+
+/// Gets the WASI sysroot path for the given simulator
+///
+/// * `version` The simulator version to get the path for
+pub fn get_wasi_sysroot_path(version: SimulatorVersion) -> Result<PathBuf> {
+    Ok(get_sdk_path(version)?.join(WASI_SYSROOT_PATH))
+}
+
+/// Gets the installed SDK version for the given simulator
+///
+/// * `version` - The simulator version to get for
+pub fn get_installed_sdk_version(version: SimulatorVersion) -> Result<Option<String>> {
+    Ok(
+        match File::open(get_sdk_path(version)?.join(VERSION_FILE_NAME)) {
+            Ok(mut file) => {
+                let mut version = String::new();
+                file.read_to_string(&mut version)?;
+                Some(version)
+            }
+            Err(_) => None,
+        },
+    )
+}
+
+/// Removes the installed SDK for the given simulator
+///
+/// * `version` The simulator version to delete the SDK for
+pub fn remove_sdk_version(version: SimulatorVersion) -> Result<()> {
+    let path = get_sdk_path(version)?;
+
+    // Clear the out directory and recreate it
+    if path.exists() {
+        fs::remove_dir_all(&path)?;
+    }
+
+    Ok(())
+}
+
+/// Installs the latest SDK version for the given simulator
+///
+/// * `version` - The simulator version to download for
+/// * `download_progress_callback` - An optional callback to report download statistics. Useful for logging. Parameters: `downloaded: u64, total: u64`
+pub fn install_latest_sdk<F>(
+    version: SimulatorVersion,
+    mut download_progress_callback: Option<F>,
+) -> Result<()>
+where
+    F: FnMut(u64, u64),
+{
+    // Clear and recreate the SDK path
+    let out_directory = get_sdk_path(version)?;
+    remove_sdk_version(version)?;
+    fs::create_dir_all(&out_directory)?;
+
+    // Get latest SDK data
+    let latest_sdk = get_latest_sdk_release(version)?;
+    let download_url = latest_sdk
+        .downloads_menu
+        .get(CORE_INSTALLER_KEY)
+        .context("can't find core installer download option")?
+        .value
+        .as_ref()
+        .context("can't find core installer download url")?;
+    let release_number = latest_sdk
+        .release_notes
+        .last()
+        .context("couldn't get latest release number")?;
+
+    // Download the installer
+    let mut response = reqwest::blocking::get(format!(
+        "{}{}",
+        if version == SimulatorVersion::Msfs2020 {
+            MSFS2020_SDK_URL
+        } else {
+            MSFS2024_SDK_URL
+        },
+        download_url
+    ))?;
+    let content_length = response
+        .content_length()
+        .context("couldn't get content length of response")?;
+
+    let mut file = Cursor::new(Vec::new());
+    loop {
+        let mut buf = [0u8; CHUNK_SIZE as usize];
+        match response.read(&mut buf) {
+            Ok(0) => break, // End of file
+            Ok(data_size) => {
+                file.write(&buf[0..data_size])?;
+                if let Some(callback) = download_progress_callback.as_mut() {
+                    callback(file.position() + data_size as u64, content_length);
+                }
+            }
+            Err(e) => return Err(anyhow!(e)),
+        }
+    }
+
+    // Parse the MSI. Some releases are zipped MSI files with external CAB files, so we need to handle that. Otherwise, everything is included in the MSI.
+    let mut msi = if download_url.ends_with(".zip") {
+        let mut zip_archive = ZipArchive::new(file)?;
+
+        // Find the MSI file in the zip listing
+        let msi_file_name = zip_archive
+            .file_names()
+            .find(|f| f.ends_with(".msi"))
+            .context("couldn't find msi in zip")?
+            .to_string();
+
+        // Read the MSI archive to a buffer and then create the package
+        let mut msi_buffer = Vec::new();
+        zip_archive
+            .by_name(&msi_file_name)?
+            .read_to_end(&mut msi_buffer)?;
+
+        let mut package = Package::open(Cursor::new(msi_buffer))?;
+
+        // Since the CAB files are external, we need to manually add them to the package. TODO: This is *really* ineff
+        for cab_file_path in zip_archive
+            .file_names()
+            .filter_map(|f| {
+                if f.ends_with(".cab") {
+                    Some(f.to_string())
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>()
+        {
+            // Get the file name from the path
+            let cab_file_name = PathBuf::from(&cab_file_path)
+                .file_name()
+                .context("couldn't get file name")?
+                .to_str()
+                .context("couldn't convert to str")?
+                .to_string();
+
+            // Read the cab archive to a buffer
+            let mut cab_buffer = Vec::new();
+            zip_archive
+                .by_name(&cab_file_path)?
+                .read_to_end(&mut cab_buffer)?;
+
+            // Write the stream
+            let mut stream = package.write_stream(&cab_file_name)?;
+            stream.write_all(&cab_buffer)?;
+        }
+
+        package
+    } else {
+        Package::open(file)?
+    };
+
+    // Query the MSI tables for info on file and folder names (see https://learn.microsoft.com/en-us/windows/win32/msi/database-tables for info on the values)
+    let query = Select::table("File")
+        .inner_join(
+            Select::table("Component"),
+            Expr::col("Component.Component").eq(Expr::col("File.Component_")),
+        )
+        .columns(&["File.File", "File.FileName", "Component.Directory_"]);
+    let files = msi.select_rows(query)?.collect::<Vec<_>>();
+    let directories = msi
+        .select_rows(Select::table("Directory").columns(&[
+            "Directory",
+            "Directory_Parent",
+            "DefaultDir",
+        ]))?
+        .collect::<Vec<_>>();
+
+    // Create a map of the file ID to the full output relative path (e.g. filFQCSYDXD6IK3UAB8101TGG3B0387F7ZD to ./Foo/Bar/Baz.qux)
+    let mut file_map = HashMap::new();
+    for file in files {
+        let file_id = file["File.File"].as_str().context("couldn't get file id")?;
+        let file_name = get_long_file_name(
+            file["File.FileName"]
+                .as_str()
+                .context("couldn't get file name")?,
+        )?;
+        let directory = get_directory_parent(
+            file["Component.Directory_"]
+                .as_str()
+                .context("couldn't get file name")?,
+            &directories,
+        )?;
+        file_map.insert(file_id.to_string(), directory.join(file_name));
+    }
+
+    // Write version file
+    let mut version_file = File::create(out_directory.join(VERSION_FILE_NAME))?;
+    version_file.write_all(release_number.as_bytes())?;
+
+    // Write SDK files
+    let extract_from = if version == SimulatorVersion::Msfs2020 {
+        MSFS2020_SDK_EXTRACT_FROM
+    } else {
+        MSFS2024_SDK_EXTRACT_FROM
+    };
+    // A more efficient way would be to find the stream associated with a file, but that is not possible. Given that, we must loop over all streams
+    for stream_name in msi.streams().collect::<Vec<_>>() {
+        let stream = msi.read_stream(&stream_name)?;
+        let mut cabinet = match Cabinet::new(stream) {
+            Ok(cabinet) => cabinet,
+            Err(_) => continue, // Not a cabinet file
+        };
+        // Since there is a weird ownership model of the crate we use, we need to go ahead and extract all the file names
+        let files = cabinet
+            .folder_entries()
+            .flat_map(|f| f.file_entries())
+            .map(|f| f.name().to_string())
+            .collect::<Vec<_>>();
+        for cab_file_name in files {
+            // cab_file_name will be the file identifier, which we will query from the file path hashmap
+            let entry = file_map
+                .get(&cab_file_name)
+                .context("couldn't find mapped file name")?;
+
+            // Only extract the SDK files we care about
+            if entry
+                .as_os_str()
+                .to_str()
+                .context("couldn't convert to str")?
+                .starts_with(extract_from)
+            {
+                // Calculate the path relative to the folder we are extracting
+                let out_file_path = out_directory.join(entry.strip_prefix(extract_from)?);
+                // Ensure directories exist
+                let parent = out_file_path.parent().context("could not get parent")?;
+                if !parent.exists() {
+                    fs::create_dir_all(parent)?;
+                }
+                // Write
+                let mut file = File::create(out_file_path)?;
+                let mut data = cabinet.read_file(&cab_file_name)?;
+                io::copy(&mut data, &mut file)?;
+            }
+        }
+    }
+
+    Ok(())
+}
